f64beb639db0823304d3cc8c7c1170c21f075625
MarkSant <Santana.marco.a@gmail.com>
2020-04-14 15:44:36 -0300

    Implementando Insert()

--- src/modelDAOImpl/SellerDAOjdbc.java
+++ src/modelDAOImpl/SellerDAOjdbc.java
@@ -17,7 +17,35 @@ public class SellerDAOjdbc implements SellerDAO
 		@Override
 		public void insert(Seller obj)
 		{
-				// TODO: Implement this method
+				PreparedStatement st = null;
+				try {
+						conn.prepareStatement( " INSERT INTO seller "
+						+ "(Name, Email, BjrthDate, BaseSalary, DepartmentId) "
+						+ "VALUES "
+						+ "(?, ?, ?, ?, ?)",
+						Statement.RETURN_GENERATED_KEYS);
+						st.setString(1, obj.getName());
+						st.setString(2, obj.getEmail());
+						st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+						st.setDouble(4, obj.getBaseSalary());
+						st.setInt(5, obj.getDepartment().getId());
+					
+						int rowsAffected = st.executeUpdate();
+						if(rowsAffected > 0) {
+								ResultSet rs = st.getGeneratedKeys();
+								if( rs.next()) {
+										int id = rs.getInt(1);
+										obj.setId(id);
+								}
+								DB.closeResultSet(rs);
+						} else {
+								throw new DbException("No row affected");
+						}
+				} catch (SQLException e) {
+						throw new DbException(e.getMessage());
+				} finally {
+						DB.closeStatement(st);
+				}
 		}
 
 		@Override
