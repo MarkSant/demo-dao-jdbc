c349938e6a95ab3e3828078765b24c6d857fe15f
MarkSant <Santana.marco.a@gmail.com>
2020-04-14 11:55:09 -0300

    Seller entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
+import modelEntities.*;
+import java.util.*;
 
-public class Program
-{
+public class Program {
 		,public static void main(String[] args) {
+				Department department = new Department(1, "Books");
+				Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
+				System.out.println(department);
+				System.out.println(seller);
 				
 		}
 }
